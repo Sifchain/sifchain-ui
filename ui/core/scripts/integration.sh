49cab3644ef6292efa9a5c9b8b827823ce105e60
Rudi Yardley <contact@rudiyardley.com>
2021-06-03 14:17:23 +1000

    Sort out test ordering

--- ui/core/scripts/integration.sh
+++ ui/core/scripts/integration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # insert args before flags allows us to target specific tests
-yarn jest $@ --coverage --runInBand --projects jest-integration.config.js
+yarn jest $@ --passWithNoTests --coverage --runInBand --projects jest-integration.config.js
--- ui/core/scripts/unit.sh
+++ ui/core/scripts/unit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # insert args before flags allows us to target specific tests
-yarn jest $@ --coverage --projects jest-unit.config.js
+yarn jest $@ --passWithNoTests --coverage --projects jest-unit.config.js
