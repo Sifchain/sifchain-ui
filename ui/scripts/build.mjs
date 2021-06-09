8c166e00017953322157519b37c542335188d410
Rudi Yardley <contact@rudiyardley.com>
2021-06-09 16:17:30 +1000

    Need to get build responding to tag or UI test will fail

--- ui/scripts/build.mjs
+++ ui/scripts/build.mjs
@@ -4,7 +4,7 @@ import { lint, setupStack } from "./lib.mjs";
 import { arg } from "./lib.mjs";
 
 const args = arg(
-  { "--no-setup": Boolean },
+  { "--no-setup": Boolean, "--tag": String, "-t:": "--tag" },
   `
 Usage: 
 
@@ -14,8 +14,8 @@ Build the frontend to the ./app/dist folder.
 
 Options: 
 
---no-setup - Don't run the docker setup scripts. This can be useful when setup and ABI extraction has already occured say with vercel for example.
-
+--no-setup      Don't run the docker setup scripts. This can be useful when setup and ABI extraction has already occured say with vercel for example.
+--tag, -t       Tag to use for extracting assets such as contract ABIs
 `,
 );
 
@@ -23,7 +23,7 @@ const core = resolve(__dirname, "../core");
 const app = resolve(__dirname, "../app");
 
 if (!args["--no-setup"]) {
-  await setupStack();
+  await setupStack(args["--tag"]);
 }
 await lint();
 await $`cd ${core} && yarn build`;
