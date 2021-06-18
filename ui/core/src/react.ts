bc009e14b0a4333f9fdb560078d5963b67bd25b5
Rudi Yardley <contact@rudiyardley.com>
2021-06-18 12:21:02 +1000

    Share state with React (#92)

--- ui/core/src/react.ts
+++ ui/core/src/react.ts
@@ -1,14 +1,50 @@
-import { useState, useCallback } from "react";
-type Api = { increment: () => void; decrement: () => void };
-export function useSifchain(): [number, Api] {
-  const [count, setState] = useState(0);
-  const increment = useCallback(() => {
-    setState((c) => c + 1);
+import {
+  createStore,
+  createServices,
+  createUsecases,
+  getConfig,
+} from "./index";
+import { useEffect, useState } from "react";
+import { Store } from "./store";
+import { effect, stop, toRaw } from "@vue/reactivity";
+
+type Usecases = ReturnType<typeof createUsecases>;
+type Bus = ReturnType<typeof createServices>["bus"];
+type Api = Usecases["clp"] &
+  Usecases["peg"] &
+  Usecases["reward"] &
+  Usecases["wallet"]["eth"] &
+  Usecases["wallet"]["sif"] & { bus: () => Bus };
+
+export function useSifchain(): [Store | undefined, Api | undefined] {
+  const [storeObject, setState] = useState<Store | undefined>(undefined);
+  const [api, setApi] = useState<Api | undefined>(undefined);
+
+  useEffect(() => {
+    console.log("Use effect");
+
+    const config = getConfig();
+    const services = createServices(config);
+    const store = createStore();
+    const usecases = createUsecases({ store, services });
+    const api = {
+      ...usecases.clp,
+      ...usecases.peg,
+      ...usecases.reward,
+      ...usecases.wallet.eth,
+      ...usecases.wallet.sif,
+      bus: () => services.bus,
+    };
+
+    setApi(api);
+    const e = effect(() => {
+      // This is highly inefficient for React we need to be able to ask for specific keys to attach to render
+      JSON.stringify(store);
+      setState(Object.assign({}, toRaw(store)));
+    });
+
+    return () => stop(e);
   }, []);
 
-  const decrement = useCallback(() => {
-    setState((c) => c - 1);
-  }, []);
-
-  return [count, { increment, decrement }];
+  return [storeObject, api];
 }
--- ui/react-example/pages/index.tsx
+++ ui/react-example/pages/index.tsx
@@ -1,11 +1,7 @@
+import JSONTree from 'react-json-tree';
 import { useSifchain } from "ui-core/lib/react";
+
 export default function Home() {
-  const [count, api] = useSifchain();
-  return (
-    <div>
-      <div>{count}</div>
-      <button onClick={api.increment}>+</button>
-      <button onClick={api.decrement}>-</button>
-    </div>
-  );
+  const [json, api] = useSifchain();
+  return <code><JSONTree data={json} /></code>
 }
