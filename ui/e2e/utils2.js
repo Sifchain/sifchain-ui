59310b8e868b8b502340b6922fe317fa861260fa
Piotr Gesicki <piotr.gesicki@gmail.com>
2021-06-23 22:17:08 +0200

    fix mkdir if it exists

--- ui/e2e/utils2.js
+++ ui/e2e/utils2.js
@@ -1,7 +1,9 @@
 const mkdirp = require("mkdirp");
+const fs = require("fs");
 
 async function takeScreenshots(name, context) {
   if (context) {
+    const screenshotsDir = "./screenshots";
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getUTCMonth() + 1;
@@ -16,7 +18,9 @@ async function takeScreenshots(name, context) {
       "_",
     )}-${dateString}`;
 
-    await mkdirp("screenshots");
+    if (!fs.existsSync(screenshotsDir)) {
+      await mkdirp(screenshotsDir);
+    }
 
     const pages = await context.pages();
     await pages.forEach(async (page, index) => {
