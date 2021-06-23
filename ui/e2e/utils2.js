9b5dd273bd94f8ed6d79492003172ef9969e5871
Piotr Gesicki <piotr.gesicki@gmail.com>
2021-06-23 22:31:46 +0200

    fix mkdir when exists

--- ui/e2e/utils2.js
+++ ui/e2e/utils2.js
@@ -1,7 +1,9 @@
 const mkdirp = require("mkdirp");
+const fs = require("fs");
 
 async function takeScreenshots(name, context) {
   if (context) {
+    const screenshotsDir = "./screenshots";
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getUTCMonth() + 1;
@@ -11,12 +13,14 @@ async function takeScreenshots(name, context) {
     const sec = date.getUTCSeconds();
     const dateString = `${year}-${month}-${dateOfMonth}-${hour}-${minute}-${sec}`;
 
-    const screenshotPath = `screenshots/${name.replace(
+    const screenshotPath = `${screenshotsDir}/${name.replace(
       / /g,
       "_",
     )}-${dateString}`;
 
-    await mkdirp("screenshots");
+    if (!fs.existsSync(screenshotsDir)) {
+      await mkdirp(screenshotsDir);
+    }
 
     const pages = await context.pages();
     await pages.forEach(async (page, index) => {
