7c94f36e9d336dca7e5e4dff3f97ba2ddbbac068
Piotr Gesicki <piotr.gesicki@gmail.com>
2021-06-25 17:25:57 +0200

    add waiting before approving keplr connection for swap tests

--- ui/e2e/swap.test.js
+++ ui/e2e/swap.test.js
@@ -41,6 +41,7 @@ beforeAll(async () => {
 
   // once keplr has finished setup, connection page will be invoked automatically
   await context.waitForEvent("page");
+  await page.waitForTimeout(500);
 
   await connectKeplrAccount();
   await connectMetaMaskAccount();
