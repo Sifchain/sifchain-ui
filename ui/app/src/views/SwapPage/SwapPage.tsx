cf2c063158441c11c241b70bde3772128ab0432b
Andrew Joslin <andrewtjoslin@gmail.com>
2021-07-05 15:42:16 -0700

    Add PageCard

--- ui/app/src/views/SwapPage/SwapPage.tsx
+++ ui/app/src/views/SwapPage/SwapPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, TransitionGroup } from "vue";
 import NavIconVue from "@/componentsLegacy/NavSidePanel/NavIcon.vue";
+import PageCard from '@/components/PageCard'
 import { useSwapPageModule } from "./useSwapPageModule";
 import { TokenInputGroup } from "./TokenInputGroup";
 import { useSwapPageData } from "./useSwapPageData--old";
@@ -21,7 +22,7 @@ export default defineComponent({
     const core = useCore();
     console.log(data.fromSymbol.value, data.toSymbol.value);
     return () => (
-      <div class="flex justify-start flex-col items-center bg-black relative right-0 left-[560px] w-[531px] rounded-[10px] p-[20px]">
+      <PageCard>
         <div class="h-[42px] w-full flex-row flex justify-start items-center">
           <NavIconVue icon="swap" class="w-[32px] h-[32px]" active />
           <span class="text-accent-base font-sans text-[26px] ml-[10px] font-semibold">
@@ -131,7 +132,7 @@ export default defineComponent({
             </div>
           </div>
         </div>
-      </div>
+      </PageCard>
     );
   },
 });
