3ed1dfc7991ac8d6fabc4306e5caaf8e46746e38
McCall Alexander <mccallalexander96@gmail.com>
2021-07-07 17:17:54 -0600

    enabled devtools

--- ui/app/src/main.ts
+++ ui/app/src/main.ts
@@ -5,6 +5,7 @@ import "./scss/index.css";
 
 const app = createApp(App);
 if (process.env.NODE_ENV === "development") {
+  app.config.performance = true;
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   app.config.devtools = true;
