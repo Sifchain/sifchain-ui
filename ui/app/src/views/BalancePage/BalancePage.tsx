ce3dd794753eb59f1fdfbbeffd86c6f5958ec308
McCall Alexander <mccallalexander96@gmail.com>
2021-07-15 23:52:18 -0600

    added sticky table header on balances

--- ui/app/src/views/BalancePage/BalancePage.tsx
+++ ui/app/src/views/BalancePage/BalancePage.tsx
@@ -31,15 +31,23 @@ export default defineComponent({
         sortBy: "name" as BalancePageState["sortBy"],
         class: "text-left",
         defaultSortBy: "asc",
+        ref: ref<HTMLElement>(),
       },
       {
         name: "Sifchain Balance",
         sortBy: "balance" as BalancePageState["sortBy"],
         class: "text-right",
         defaultSortBy: "desc",
+        ref: ref<HTMLElement>(),
       },
     ];
-
+    const colStyles = computed(() => {
+      return columns.map((col) => {
+        return {
+          width: `${col.ref.value?.getBoundingClientRect().width}px`,
+        };
+      });
+    });
     let isDisabled = false;
     return () => (
       <>
@@ -49,59 +57,77 @@ export default defineComponent({
           iconName="navigation/balances"
           class="w-[800px]"
           headerContent={
-            <div class="bg-gray-input_outline h-8 relative flex items-center rounded-lg overflow-hidden">
-              <AssetIcon
-                size={20}
-                icon="interactive/search"
-                class={[`ml-3 w-4 h-4`, isDisabled ? "text-[#6E6E6E]" : ""]}
-              />
-              <input
-                type="search"
-                placeholder="Search Token..."
-                value={state.searchQuery}
-                onInput={(e: Event) => {
-                  state.searchQuery = (e.target as HTMLInputElement).value;
-                }}
-                class="box-border w-full absolute top-0 bottom-0 left-0 right-0 pl-8 pr-3 h-full bg-transparent outline-none text-white font-sans font-medium"
-              />
+            <div class="w-full">
+              <div class="bg-gray-input_outline h-8 relative flex items-center rounded-lg overflow-hidden">
+                <AssetIcon
+                  size={20}
+                  icon="interactive/search"
+                  class={[`ml-3 w-4 h-4`, isDisabled ? "text-[#6E6E6E]" : ""]}
+                />
+                <input
+                  type="search"
+                  placeholder="Search Token..."
+                  value={state.searchQuery}
+                  onInput={(e: Event) => {
+                    state.searchQuery = (e.target as HTMLInputElement).value;
+                  }}
+                  class="box-border w-full absolute top-0 bottom-0 left-0 right-0 pl-8 pr-3 h-full bg-transparent outline-none text-white font-sans font-medium"
+                />
+              </div>
+              <div class="h-4 w-full" />
+              <div class="pb-[5px] mb-[-5px] opacity-50 w-full flex flex-row justify-start">
+                <div class="w-full flex flex-row justify-start font-medium text-xs align-text-bottom">
+                  {columns.map((column, index) => (
+                    <div
+                      style={colStyles.value[index]}
+                      class={[column.class]}
+                      key={column.name}
+                    >
+                      <div
+                        class="inline-flex items-center cursor-pointer opacity-50 hover:opacity-60"
+                        onClick={() => {
+                          if (state.sortBy === column.sortBy) {
+                            state.sortDirection =
+                              state.sortDirection === "asc" ? "desc" : "asc";
+                          } else {
+                            state.sortDirection = column.defaultSortBy as BalancePageState["sortDirection"];
+                          }
+                          state.sortBy = column.sortBy;
+                        }}
+                      >
+                        {column.name}
+                        {state.sortBy === column.sortBy && (
+                          <AssetIcon
+                            icon="interactive/arrow-down"
+                            class="transition-all w-[12px] h-[12px]"
+                            style={{
+                              transform:
+                                state.sortDirection === "asc"
+                                  ? "rotate(0deg)"
+                                  : "rotate(180deg)",
+                            }}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           }
         >
-          <div class="h-4 w-full" />
           <table class="w-full">
-            <thead class="opacity-50">
-              <tr class="font-medium sticky text-xs align-text-bottom">
-                {columns.map((column) => (
-                  <td class={[column.class]} key={column.name}>
-                    <div
-                      class="inline-flex items-center cursor-pointer opacity-50 hover:opacity-60"
-                      onClick={() => {
-                        if (state.sortBy === column.sortBy) {
-                          state.sortDirection =
-                            state.sortDirection === "asc" ? "desc" : "asc";
-                        } else {
-                          state.sortDirection = column.defaultSortBy as BalancePageState["sortDirection"];
-                        }
-                        state.sortBy = column.sortBy;
-                      }}
-                    >
-                      {column.name}
-                      {state.sortBy === column.sortBy && (
-                        <AssetIcon
-                          icon="interactive/arrow-down"
-                          class="transition-all w-[12px] h-[12px]"
-                          style={{
-                            transform:
-                              state.sortDirection === "asc"
-                                ? "rotate(0deg)"
-                                : "rotate(180deg)",
-                          }}
-                        />
-                      )}
-                    </div>
-                  </td>
+            <thead>
+              <tr class="font-medium text-xs align-text-bottom">
+                {columns.map((column, index) => (
+                  <td
+                    ref={column.ref}
+                    class={[column.class]}
+                    key={column.name}
+                  ></td>
                 ))}
                 <td /> {/* Actions */}
+                <td></td>
               </tr>
             </thead>
             <tbody class="w-full relative">
