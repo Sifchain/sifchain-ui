b7181bb87b3d722b08295759edaf36c9a1ee15af
Andrew Joslin <andrewtjoslin@gmail.com>
2021-08-10 16:22:04 -0700

    Fix photon not being burnt for eth=>sifchain peg

--- ui/core/src/usecases/peg/unpeg.ts
+++ ui/core/src/usecases/peg/unpeg.ts
@@ -4,7 +4,7 @@ import { IAssetAmount, Network, TransactionStatus } from "../../entities";
 import { Services } from "../../services";
 import { Store } from "../../store";
 import { calculateUnpegFee } from "./utils/calculateExportFee";
-import { isOriginallySifchainNativeToken } from "./utils/isOriginallySifchainNativeToken";
+import { isOriginallyNetworkNativeToken } from "./utils/isOriginallyNetworkNativeToken";
 
 type UnpegServices = {
   ethbridge: Pick<Services["ethbridge"], "lockToEthereum" | "burnToEthereum">;
@@ -66,7 +66,7 @@ export function Unpeg(services: UnpegServices, store: UnpegStore) {
       }
       return;
     }
-    const lockOrBurnFn = isOriginallySifchainNativeToken(assetAmount.asset)
+    const lockOrBurnFn = isOriginallyNetworkNativeToken(assetAmount.asset)
       ? services.ethbridge.lockToEthereum
       : services.ethbridge.burnToEthereum;
 
--- ui/core/src/usecases/peg/utils/isOriginallySifchainNativeToken.ts
+++
@@ -1,5 +0,0 @@
-import { IAsset } from "../../../entities";
-
-export function isOriginallySifchainNativeToken(asset: IAsset) {
-  return ["erowan", "rowan", "uphoton"].includes(asset.symbol);
-}
