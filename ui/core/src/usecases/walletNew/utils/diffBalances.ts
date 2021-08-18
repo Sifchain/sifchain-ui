4b5781a9be1641b3687f5a0682f152965adf2443
Andrew Joslin <andrewtjoslin@gmail.com>
2021-08-18 07:53:49 -0700

    Fix diffBalances

--- ui/core/src/usecases/walletNew/utils/diffBalances.ts
+++ ui/core/src/usecases/walletNew/utils/diffBalances.ts
@@ -5,13 +5,8 @@ export default function diffBalances(b1: IAssetAmount[], b2: IAssetAmount[]) {
     balances.map((b) => {
       return `${b.symbol}_${b.amount.toBigInt().toString()}`;
     });
-  const mapped1 = getAssetAmountKeys(b1);
-  const mapped2 = getAssetAmountKeys(b2);
+  const mapped1 = getAssetAmountKeys(b1).join(" ");
+  const mapped2 = getAssetAmountKeys(b2).join(" ");
 
-  return (
-    mapped1.length === mapped2.length &&
-    mapped1.every((item, index) => {
-      return mapped2[index] === item;
-    })
-  );
+  return mapped1 !== mapped2;
 }
