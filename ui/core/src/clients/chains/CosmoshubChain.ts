17eacf1f56f0c46bc053043d67a0cee55e1e6277
Andrew Joslin <andrewtjoslin@gmail.com>
2021-08-23 14:34:21 -0700

    fix cosmoshub links

--- ui/core/src/clients/chains/CosmoshubChain.ts
+++ ui/core/src/clients/chains/CosmoshubChain.ts
@@ -1,4 +1,8 @@
 import { Chain, Network } from "../../entities";
 import { BaseChain } from "./BaseChain";
 
-export class CosmoshubChain extends BaseChain implements Chain {}
+export class CosmoshubChain extends BaseChain implements Chain {
+  getBlockExplorerUrlForTxHash(hash: string) {
+    return urlJoin(this.chainConfig.blockExplorerUrl, "txs", hash);
+  }
+}
