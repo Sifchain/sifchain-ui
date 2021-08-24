7912930e5509c58e767ffe8b84e339a82ce7cfb9
Andrew Joslin <andrewtjoslin@gmail.com>
2021-08-24 15:08:15 -0700

    Fix sentinelchain config

--- ui/core/src/clients/chains/SentinelChain.ts
+++ ui/core/src/clients/chains/SentinelChain.ts
@@ -1,9 +1,9 @@
 import { Chain, Network, IAssetAmount } from "../../entities";
 import { urlJoin } from "url-join-ts";
-import { CosmoshubChain } from "./CosmoshubChain";
 import { calculateIBCExportFee } from "../../utils/ibcExportFees";
+import { BaseChain } from "./_BaseChain";
 
-export class SentinelChain extends CosmoshubChain implements Chain {
+export class SentinelChain extends BaseChain implements Chain {
   getBlockExplorerUrlForTxHash(hash: string) {
     return urlJoin(this.chainConfig.blockExplorerUrl, "transactions", hash);
   }
