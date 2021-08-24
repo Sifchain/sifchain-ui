86e10f21363fd6d91585091d4977745882ccadc4
Andrew Joslin <andrewtjoslin@gmail.com>
2021-08-24 16:17:09 -0700

    Testnet bgs

--- ui/app/src/componentsLegacy/Layout/LayoutBackground.tsx
+++ ui/app/src/componentsLegacy/Layout/LayoutBackground.tsx
@@ -11,7 +11,14 @@ import {
 
 const layoutBgKey = "layout_bg";
 const layoutBgDefault = "default";
-const LAYOUT_BACKGROUNDS = [
+
+type LayoutBg = {
+  key: string;
+  src: string[];
+  thumb: string;
+  render?: (src: string) => JSX.Element;
+};
+const LAYOUT_BACKGROUNDS: LayoutBg[] = [
   {
     key: "default",
     src: [require("@/assets/backgrounds/default.webp")],
@@ -72,8 +79,39 @@ const LAYOUT_BACKGROUNDS = [
     ],
     thumb: require("@/assets/backgrounds/view-thumb.webp"),
   },
+  {
+    key: "dark",
+    src: [
+      require("@/assets/backgrounds/dark-coin-1x.webp"),
+      require("@/assets/backgrounds/dark-coin-2x.webp"),
+      require("@/assets/backgrounds/dark-coin-4x.webp"),
+    ],
+    thumb: require("@/assets/backgrounds/dark-coin-thumb.webp"),
+    render(src: string) {
+      return (
+        <div class="w-full h-full relative bg-black">
+          <img src={src} class="w-[42%]" />
+        </div>
+      );
+    },
+  },
+  {
+    key: "gold",
+    src: [
+      require("@/assets/backgrounds/gold-1x.webp"),
+      require("@/assets/backgrounds/gold-2x.webp"),
+      require("@/assets/backgrounds/gold-4x.webp"),
+    ],
+    thumb: require("@/assets/backgrounds/gold-coin-thumb.webp"),
+    render(src: string) {
+      return (
+        <div class="w-full h-full relative bg-[#3B7FBA] flex items-center justify-end">
+          <img src={src} class="w-[47%]" />
+        </div>
+      );
+    },
+  },
 ];
-
 export default defineComponent({
   name: "LayoutBackground",
   setup() {
@@ -102,31 +140,44 @@ export default defineComponent({
       window.removeEventListener("resize", updateWidth);
     });
 
-    const srcRef = computed(() => {
-      const bg =
+    const getSrc = (bg: LayoutBg) => {
+      const base = 1440;
+      const index =
+        widthRef.value > base * 2 ? 2 : widthRef.value > base * 1.5 ? 1 : 0;
+      return bg?.src[index] || bg?.src[0];
+    };
+
+    const bgRef = computed(() => {
+      return (
         LAYOUT_BACKGROUNDS.find(
           (item) => item.key === backgroundKeyRef.value,
-        ) || LAYOUT_BACKGROUNDS.find((item) => item.key === layoutBgDefault);
+        ) || LAYOUT_BACKGROUNDS.find((item) => item.key === layoutBgDefault)
+      );
+    });
 
-      const base = 1440;
-
-      const index =
-        widthRef.value > base * 3 ? 2 : widthRef.value > base * 2 ? 1 : 0;
-      return bg?.src[index] || bg?.src[0];
+    const srcRef = computed(() => {
+      if (bgRef.value) return getSrc(bgRef.value);
     });
 
     return () => (
       <>
         <div
+          id="layout-bg"
           class="z-[-1] w-full h-[100vh] fixed top-0 left-0 transition-all duration-500"
-          style={{
-            backgroundImage: `url(${srcRef.value})`,
-            backgroundSize: "cover",
-            backgroundPosition: "top center",
-            backgroundAttachment: "fixed",
-            backgroundRepeat: "no-repeat",
-          }}
-        />
+          style={
+            !bgRef.value?.render
+              ? {
+                  backgroundImage: `url(${srcRef.value})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "top center",
+                  backgroundAttachment: "fixed",
+                  backgroundRepeat: "no-repeat",
+                }
+              : {}
+          }
+        >
+          {bgRef.value?.render && bgRef.value.render(srcRef.value || "")}
+        </div>
         <div class="absolute bottom-4 right-4 w-[36px] h-[36px]">
           <Tooltip
             placement="top-end"
@@ -151,10 +202,18 @@ export default defineComponent({
                       "w-1/3 p-[8px] relative rounded-sm cursor-pointer hover:bg-gray-600",
                     ]}
                   >
-                    <img class="w-full" src={bg.thumb} />
-                    <div
-                      class={["bg-black inset-[8px] absolute bg-opacity-20"]}
-                    ></div>
+                    {bg.render ? (
+                      bg.render(getSrc(bg) || "")
+                    ) : (
+                      <>
+                        <img class="w-full" src={bg.thumb} />
+                        <div
+                          class={[
+                            "bg-black inset-[8px] absolute bg-opacity-20",
+                          ]}
+                        ></div>
+                      </>
+                    )}
                   </div>
                 ))}
               </div>
