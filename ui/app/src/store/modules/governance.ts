f31ecf9e1d28218da665942ab458442e2620ed4c
McCall Alexander <mccallalexander96@gmail.com>
2022-01-29 09:36:59 -0700

    Add PoP v4 Stable Proposal to Governance (#396)
    * removed autocomplete from token search
    
    * added excludedsymbols for proposal
    
    * updated wording to say ballot instead of vote for notification
    
    * updated voting features to submit displaySymbols, filter accordingly, hide token selection modal when complete, and open modal via query parameter
    
    * added notif update and included start datetime

--- ui/app/src/store/modules/governance.ts
+++ ui/app/src/store/modules/governance.ts
@@ -28,6 +28,8 @@ type Proposal = {
   heading: string;
   description: string;
   voteType: "pools" | "yes_no";
+  maxBallots: number;
+  excludedSymbols?: string[];
 };
 
 const proposals: Proposal[] = [
@@ -40,6 +42,7 @@ const proposals: Proposal[] = [
     heading: `Should Sif's Expansion continue for the next four weeks?`,
     description: `If you would like to continue this rewards program vote Yes.\nIf you would like to stop this rewards program vote No.`,
     voteType: "yes_no",
+    maxBallots: 1,
   },
   {
     id: "4pools_12_21",
@@ -50,7 +53,37 @@ const proposals: Proposal[] = [
     heading: `Sif's Expansion Pools - Vote for 300% APR`,
     description: `Vote for 4 pools to have ~300% APR for the next 4 weeks. As a reminder, 5 pools in total will have an APR of ~300% for the next four weeks (ATOM:ROWAN and 4 selected by the majority in this vote). All other pools will maintain a 100% APR.  Every 4 weeks, users will execute this vote.`,
     voteType: "pools",
+    maxBallots: 4,
   },
+  {
+    id: "4pools_01_29_22",
+    address: "sif1seftxu8l6v7d50ltm3v7hl55jlyxrps53rmjl8",
+    startDateTime: new Date(1643472000000),
+    // startDateTime: new Date(Date.now() - 100000000),
+    endDateTime: new Date(1643472000000 + 72 * 60 * 60 * 1000),
+    heading: `Sif's Expansion Stablecoin Bonus`,
+    title: `Stablecoin Vote`,
+    description: `Polls are now open for the Pools of the People v4 - Stable Coin Vote. The voting period will end on 2/1 at 8am PST. Please select 1 stablecoin pool.`,
+    voteType: "pools",
+    maxBallots: 1,
+    excludedSymbols: ["rowan", "uatom", "ceth", "cust"],
+  },
+  // {
+  //   id: "4pools_01_29_22",
+  //   address: "sif1seftxu8l6v7d50ltm3v7hl55jlyxrps53rmjl8",
+  //   // startDateTime: new Date(1643472000000),
+  //   startDateTime: new Date(Date.now() - 100000000),
+  //   endDateTime: new Date(1643472000000 + 72 * 60 * 60 * 1000),
+  //   heading: `Sif's Expansion Bonus (v4)`,
+  //   title: `Stablecoin Vote`,
+  //   description: `Polls are now open for the Pools of the People v4 - Bonus Coin Vote. The voting period will end on 2/4 at 8am PST.`,
+  //   voteType: "pools",
+  //   maxBallots: 1,
+  //   excludedSymbols: ['rowan', 'uatom', 'ceth']
+  //   // use display symbols instead of denoms
+  //   // add home network in future (consider omnievm)
+  //   // change address to deprecate old voting method (manual)
+  // },
 ];
 
 export const governanceStore = Vuextra.createStore({
