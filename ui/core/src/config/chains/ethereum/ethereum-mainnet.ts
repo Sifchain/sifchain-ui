0e88176f0be011e784db6f88897d625b8c377e18
Andrew Joslin <andrewtjoslin@gmail.com>
2022-02-07 20:10:32 -0800

    Remove underMaintenance

--- ui/core/src/config/chains/ethereum/ethereum-mainnet.ts
+++ ui/core/src/config/chains/ethereum/ethereum-mainnet.ts
@@ -2,7 +2,6 @@ import { EthChainConfig, Network } from "../../../entities";
 
 export const ETHEREUM_MAINNET: EthChainConfig = {
   chainType: "eth",
-  underMaintenance: true,
   chainId: "0x1", // mainnet
   network: Network.ETHEREUM,
   displayName: "Ethereum",
