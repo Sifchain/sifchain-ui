b50e9e809dc1c14d375b39332a76f563ffce24ac
Alan Soares <alanrsoars@gmail.com>
2022-02-16 14:47:12 +1300

    chore: extract test util: sanitizeNumberString

--- ui/core/src/entities/formulae.calculateSwapResult_pmtp.test.ts
+++ ui/core/src/entities/formulae.calculateSwapResult_pmtp.test.ts
@@ -1,15 +1,16 @@
 import { calculateSwapResult_pmtp } from "./formulae";
 import { Amount } from "./Amount";
 import tests from "../../../../test/test-tables/singleswap_result.json";
+import { sanitizeNumberString } from "test/utils/sanitizeNumberString";
 
-const sanitize = (x: string) => x.replace(/\_/g, "");
+const toAmount = (s: string) => Amount(sanitizeNumberString(s));
 
 describe("calculateSwapResult_pmtp", () => {
   tests.SingleSwapResult_pmtp.forEach(({ x, X, Y, wx, wy, expected }) => {
     test(`Swapping ${x}, expecting ${expected}`, () => {
-      const $x = Amount(sanitize(x)); // Swap Amount
-      const $X = Amount(sanitize(X)); // External Balance
-      const $Y = Amount(sanitize(Y)); // Native Balance
+      const $x = toAmount(x); // Swap Amount
+      const $X = toAmount(X); // External Balance
+      const $Y = toAmount(Y); // Native Balance
 
       const output = calculateSwapResult_pmtp(
         // External -> Native pool
