3b0a246ccab582e1dc35edec218fcd4c2ec075dc
Alan Soares <alanrsoars@gmail.com>
2022-03-26 12:47:03 +1300

    chore: cleanup components

---
+++ app/src/components/Flags/index.ts
@@ -0,0 +1,1 @@
+export { Flags as default } from "./Flags";
--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./scss/index.css";
-import { rootStore, vuexStore } from "./store/index";
+import { vuexStore } from "./store/index";
 
 const app = createApp(App);
 
