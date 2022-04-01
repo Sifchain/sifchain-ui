578b6d002d5e561eb77471252a0e0498568c465e
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-04-01 16:25:59 +1300

    refactor: use `Object.assign`
    for better type definition

--- core/src/entities/Asset.ts
+++ core/src/entities/Asset.ts
@@ -26,53 +26,52 @@ function isAsset(value: any): value is IAsset {
   );
 }
 
-export function Asset(assetOrSymbol: IAsset | string): ReadonlyAsset {
-  // If it is an asset then cache it and return it
-  if (isAsset(assetOrSymbol)) {
-    const key = assetOrSymbol.symbol.toLowerCase();
+export const Asset = Object.assign(
+  (assetOrSymbol: IAsset | string): ReadonlyAsset => {
+    // If it is an asset then cache it and return it
+    if (isAsset(assetOrSymbol)) {
+      const key = assetOrSymbol.symbol.toLowerCase();
 
-    // prevent overriding of existing rowan asset
-    if (assetMap.has(key) && key === "rowan") {
+      // prevent overriding of existing rowan asset
+      if (assetMap.has(key) && key === "rowan") {
+        return assetOrSymbol;
+      }
+
+      assetMap.set(key, {
+        ...assetOrSymbol,
+        displaySymbol: assetOrSymbol.displaySymbol || assetOrSymbol.symbol,
+      });
+
       return assetOrSymbol;
     }
 
-    assetMap.set(key, {
-      ...assetOrSymbol,
-      displaySymbol: assetOrSymbol.displaySymbol || assetOrSymbol.symbol,
-    });
+    // Return it from cache
+    const found = assetOrSymbol
+      ? assetMap.get(assetOrSymbol.toLowerCase())
+      : false;
 
-    return assetOrSymbol;
-  }
+    if (!found) {
+      throw new Error(
+        `Attempt to retrieve the asset with key "${assetOrSymbol}" before it had been cached.`,
+      );
+    }
 
-  // Return it from cache
-  const found = assetOrSymbol
-    ? assetMap.get(assetOrSymbol.toLowerCase())
-    : false;
-
-  if (!found) {
-    throw new Error(
-      `Attempt to retrieve the asset with key "${assetOrSymbol}" before it had been cached.`,
-    );
-  }
-
-  return found;
-}
-
-/**
- * @ignore
- */
-Asset.set = (symbol: string, asset: IAsset) => {
-  Asset(asset); // assuming symbol is same
-};
-
-/**
- * A quick way to look up an asset by symbol.
- * Pass in a string, and it will attempt to look up the asset and return it. Throws an error if the asset is not found.
- *
- * Pass in an IAsset, and it will save it for future lookups.
- *
- * @remarks This lookup is only a shortcut and does not allow you to lookup an asset by chain. For that, use Chain#lookupAsset.
- */
-Asset.get = (symbol: string) => {
-  return Asset(symbol);
-};
+    return found;
+  },
+  {
+    set: (symbol: string, asset: IAsset) => {
+      Asset(asset); // assuming symbol is same,
+    },
+    /**
+     * A quick way to look up an asset by symbol.
+     * Pass in a string, and it will attempt to look up the asset and return it. Throws an error if the asset is not found.
+     *
+     * Pass in an IAsset, and it will save it for future lookups.
+     *
+     * @remarks This lookup is only a shortcut and does not allow you to lookup an asset by chain. For that, use Chain#lookupAsset.
+     */
+    get: (symbol: string) => {
+      return Asset(symbol);
+    },
+  },
+);
