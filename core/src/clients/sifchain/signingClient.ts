8d2c2f5b94f7a9a447c255546890a206f5c35b79
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-04-21 00:38:16 +1200

    feat: add amino types support

--- core/src/clients/sifchain/signingClient.ts
+++ core/src/clients/sifchain/signingClient.ts
@@ -1,3 +1,4 @@
+import { AminoMsg } from "@cosmjs/amino";
 import {
   GeneratedType,
   isTsProtoGeneratedType,
@@ -5,6 +6,15 @@ import {
   Registry,
 } from "@cosmjs/proto-signing";
 import {
+  AminoConverters,
+  AminoTypes,
+  createAuthzAminoConverters,
+  createBankAminoConverters,
+  createDistributionAminoConverters,
+  createFreegrantAminoConverters,
+  createGovAminoConverters,
+  createIbcAminoConverters,
+  createStakingAminoConverters,
   defaultRegistryTypes as defaultStargateTypes,
   SigningStargateClient,
   SigningStargateClientOptions,
@@ -13,6 +23,10 @@ import * as clpTx from "../../generated/proto/sifnode/clp/v1/tx";
 import * as dispensationTx from "../../generated/proto/sifnode/dispensation/v1/tx";
 import * as ethBridgeTx from "../../generated/proto/sifnode/ethbridge/v1/tx";
 import * as tokenRegistryTx from "../../generated/proto/sifnode/tokenregistry/v1/tx";
+import {
+  convertToSnakeCaseDeep,
+  createAminoTypeNameFromProtoTypeUrl,
+} from "../native";
 import { DEFAULT_GAS_PRICE } from "./fees";
 
 const generateTypeUrlAndTypeRecords = (
@@ -27,20 +41,54 @@ const generateTypeUrlAndTypeRecords = (
       type: value,
     }));
 
+const createSifchainAminoConverters = (): AminoConverters => {
+  const protoTypes = [
+    clpTx,
+    dispensationTx,
+    ethBridgeTx,
+    tokenRegistryTx,
+  ].flatMap(generateTypeUrlAndTypeRecords);
+
+  return Object.fromEntries(
+    protoTypes.map((x) => [
+      x.typeUrl,
+      {
+        aminoType: createAminoTypeNameFromProtoTypeUrl(x.typeUrl),
+        toAmino: (value: any): AminoMsg => convertToSnakeCaseDeep(value),
+        fromAmino: (value: AminoMsg): any => convertToSnakeCaseDeep(value),
+      },
+    ]),
+  );
+};
+
+const createDefaultTypes = () =>
+  new AminoTypes({
+    ...createAuthzAminoConverters(),
+    ...createBankAminoConverters(),
+    ...createDistributionAminoConverters(),
+    ...createGovAminoConverters(),
+    ...createStakingAminoConverters("sif"),
+    ...createIbcAminoConverters(),
+    ...createFreegrantAminoConverters(),
+    ...createSifchainAminoConverters(),
+  });
+
+const createDefaultRegistry = () => {
+  const registry = new Registry(defaultStargateTypes);
+  [clpTx, dispensationTx, ethBridgeTx, tokenRegistryTx]
+    .flatMap(generateTypeUrlAndTypeRecords)
+    .forEach((x) => registry.register(x.typeUrl, x.type));
+  return registry;
+};
+
 export const createSigningClient = (
   rpcUrl: string,
   signer: OfflineSigner,
   options?: SigningStargateClientOptions,
-) => {
-  const registry = new Registry(defaultStargateTypes);
-
-  [clpTx, dispensationTx, ethBridgeTx, tokenRegistryTx]
-    .flatMap(generateTypeUrlAndTypeRecords)
-    .forEach((x) => registry.register(x.typeUrl, x.type));
-
-  return SigningStargateClient.connectWithSigner(rpcUrl, signer, {
-    registry,
+) =>
+  SigningStargateClient.connectWithSigner(rpcUrl, signer, {
+    registry: createDefaultRegistry(),
+    aminoTypes: createDefaultTypes(),
     gasPrice: DEFAULT_GAS_PRICE,
     ...options,
   });
-};
