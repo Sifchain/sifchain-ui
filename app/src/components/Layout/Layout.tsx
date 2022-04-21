2004640432f61cf6c86c6bbae8a943c5975cd884
Alan Soares <alanrsoars@gmail.com>
2022-04-22 10:18:59 +1200

    ui: add dismiss / don't show actions to alertbanner

--- app/src/components/Layout/Layout.tsx
+++ app/src/components/Layout/Layout.tsx
@@ -1,89 +1,9 @@
-import clsx from "clsx";
-import {
-  computed,
-  defineComponent,
-  HtmlHTMLAttributes,
-  PropType,
-  ref,
-} from "vue";
+import { defineComponent, HtmlHTMLAttributes, PropType } from "vue";
 
-import { useCurrentRouteBannerMessage } from "@/hooks/useCurrentRouteBannerMessage";
 import BetaWarningBanner from "@/components/BetaWarningBanner";
 
 import LayoutBackground from "./LayoutBackground";
-import Button from "../Button";
-
-function hashCode(str: string) {
-  let hash = 0;
-  if (str.length == 0) return hash;
-  for (let i = 0; i < str.length; i++) {
-    const char = str.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
-}
-
-const AlertBanner = defineComponent({
-  name: "AlertBanner",
-  setup() {
-    const bannerMessageRef = useCurrentRouteBannerMessage();
-
-    const isFullScreen = computed(
-      () => bannerMessageRef.value?.meta.fullScreen,
-    );
-    const messageHash = computed(() =>
-      !bannerMessageRef.value
-        ? null
-        : `sifalerts/${hashCode(bannerMessageRef.value.message).toString(16)}`,
-    );
-
-    const uiDismissed = ref(false);
-
-    const isDismissed = computed(() => {
-      if (!bannerMessageRef.value || !messageHash.value) {
-        return false;
-      }
-
-      const dismissed = Boolean(
-        JSON.parse(localStorage.getItem(messageHash.value) ?? "false"),
-      );
-
-      return dismissed;
-    });
-
-    function dismiss() {
-      if (!messageHash.value) return;
-
-      uiDismissed.value = true;
-      localStorage.setItem(messageHash.value, "true");
-    }
-
-    return () =>
-      !bannerMessageRef.value ||
-      isDismissed.value ||
-      uiDismissed.value ? null : (
-        <div
-          class={clsx(
-            "bg-info-base/60 z-10 font-semibold text-white backdrop-blur-md",
-            {
-              "absolute left-0 right-0 p-4 pt-8": !isFullScreen.value,
-              "fixed inset-0 grid h-screen place-items-center text-lg md:p-24":
-                isFullScreen.value,
-              "pointer-events-none": isFullScreen.value,
-            },
-          )}
-        >
-          <Button.Inline
-            onClick={dismiss}
-            icon="interactive/close"
-            class="absolute bottom-1 right-1 !h-6 !w-6 rounded-full"
-          />
-          <p class="max-w-6xl pr-8">{bannerMessageRef.value.message}</p>
-        </div>
-      );
-  },
-});
+import AlertBanner from "../AlertBanner";
 
 export default defineComponent({
   name: "Layout",
