078f31a53b9f7620500ec18d3a47ee5043120bbc
Grant Zukel <grant@sifchain.finance>
2022-04-28 23:04:56 -0600

    added evmos to network.ts

--- core/src/entities/Network.ts
+++ core/src/entities/Network.ts
@@ -16,6 +16,7 @@ export enum Network {
   SENTINEL = "sentinel",
   TERRA = "terra",
   EMONEY = "emoney",
+  EVMOS = "evmos",
   STARNAME = "starname",
   BITSONG = "bitsong",
   CERBERUS = "cerberus",
