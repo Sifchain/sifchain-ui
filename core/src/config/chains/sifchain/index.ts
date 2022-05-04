b46a201028f484bbc1899358a18f8a5fbd4b816f
Caner Candan <caner@candan.fr>
2022-05-05 00:33:54 +0200

    fix: 🐛 localnet config and remove 042 temp files

--- core/src/config/chains/sifchain/index.ts
+++ core/src/config/chains/sifchain/index.ts
@@ -1,12 +1,12 @@
 import { NetworkEnv } from "../../getEnv";
 import { NetEnvChainConfigLookup } from "../NetEnvChainConfigLookup";
-import { SIFCHAIN_TESTNET_042 } from "./sifchain-testnet-042-ibc";
 import { SIFCHAIN_DEVNET } from "./sifchain-devnet";
 import { SIFCHAIN_TESTNET } from "./sifchain-testnet";
 import { SIFCHAIN_MAINNET } from "./sifchain-mainnet";
+import { SIFCHAIN_LOCALNET } from "./sifchain-localnet";
 
 export default <NetEnvChainConfigLookup>{
-  [NetworkEnv.LOCALNET]: SIFCHAIN_TESTNET_042,
+  [NetworkEnv.LOCALNET]: SIFCHAIN_LOCALNET,
   [NetworkEnv.DEVNET]: SIFCHAIN_DEVNET,
   [NetworkEnv.TESTNET]: SIFCHAIN_TESTNET,
   [NetworkEnv.MAINNET]: SIFCHAIN_MAINNET,
