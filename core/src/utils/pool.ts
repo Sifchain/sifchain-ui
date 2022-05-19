09197cb202ff2dca126a98064d220d034bf13741
Alan <alanrsoars@gmail.com>
2022-05-19 14:21:50 +1200

    chore: cleanup core

--- core/src/utils/pool.ts
+++ core/src/utils/pool.ts
@@ -1,6 +1,6 @@
 import { Asset, IAsset } from "../entities";
 
-export const createPoolKey = (a: IAsset | string, b: IAsset | string) => {
+export function createPoolKey(a: IAsset | string, b: IAsset | string) {
   if (typeof a === "string") a = Asset.get(a);
   if (typeof b === "string") b = Asset.get(b);
 
@@ -8,4 +8,4 @@ export const createPoolKey = (a: IAsset | string, b: IAsset | string) => {
     .map((asset) => asset.symbol.toLowerCase())
     .sort()
     .join("_");
-};
+}
