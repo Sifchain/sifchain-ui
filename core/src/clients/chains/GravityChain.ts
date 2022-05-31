fb3399c7dba76cc623285ccf40f17f2d8f57f662
Alan <alanrsoars@gmail.com>
2022-05-31 14:10:43 +1200

    fix: Gravity's block explorer url

--- core/src/clients/chains/GravityChain.ts
+++ core/src/clients/chains/GravityChain.ts
@@ -5,7 +5,7 @@ import { BaseChain } from "./_BaseChain";
 
 export class GravityChain extends BaseChain implements Chain {
   getBlockExplorerUrlForTxHash(hash: string) {
-    return urlJoin(this.chainConfig.blockExplorerUrl, "tx", hash);
+    return urlJoin(this.chainConfig.blockExplorerUrl, "txs", hash);
   }
   getBlockExplorerUrlForAddress(hash: string) {
     return urlJoin(this.chainConfig.blockExplorerUrl, "account", hash);
