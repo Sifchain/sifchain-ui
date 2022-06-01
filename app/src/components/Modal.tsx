5b77fd3b20596d6b264f2f0c28abbc3cc43c4878
Alan <alanrsoars@gmail.com>
2022-06-01 14:19:27 +1200

    ui: fix modal styles

--- app/src/components/Modal.tsx
+++ app/src/components/Modal.tsx
@@ -84,7 +84,7 @@ export default defineComponent({
         >
           <div
             class={[
-              "relative flex-col items-center justify-start",
+              "relative flex-col items-center justify-start md:w-[578px]",
               "rounded-lg bg-black p-4 text-white",
               props.class,
             ]}
