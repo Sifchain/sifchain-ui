2ede436940905a9092755ce5b904b97f17585585
Alan <alanrsoars@gmail.com>
2022-06-08 11:19:46 +1200

    chore: fix localnet configs

--- core/src/config/getEnv.ts
+++ core/src/config/getEnv.ts
@@ -41,8 +41,8 @@ export const profileLookup: ProfileLookup = {
   },
   [NetworkEnv.LOCALNET]: {
     tag: NetworkEnv.LOCALNET,
-    ethAssetTag: "ethereum.localnet",
-    sifAssetTag: "sifchain.localnet",
+    ethAssetTag: "ethereum.devnet",
+    sifAssetTag: "sifchain.devnet",
     cosmoshubAssetTag: "cosmoshub.testnet",
   },
 } as const;
