30a685c6095672a557c15dd3057bc6b4cfe5955c
Alan <alanrsoars@gmail.com>
2022-06-09 11:03:38 +1200

    chore: add stargaze mainnet chain config

---
+++ core/src/config/chains/stargaze/stargaze-mainnet.ts
@@ -0,0 +1,59 @@
+import { Network, IBCChainConfig } from "../../../entities";
+
+export const STARGAZE_MAINNET: IBCChainConfig = {
+  chainType: "ibc",
+  network: Network.STARGAZE,
+  displayName: "Stargaze",
+  blockExplorerUrl: "https://www.mintscan.io/stargaze",
+  nativeAssetSymbol: "ustarx",
+  chainId: "stargaze-1",
+  rpcUrl: "https://proxies.sifchain.finance/api/stargaze-1/rpc",
+  restUrl: "https://proxies.sifchain.finance/api/stargaze-1/rest",
+  denomTracesPath: "/ibc/apps/transfer/v1/denom_traces",
+  features: {
+    erc20Transfers: true,
+  },
+  keplrChainInfo: {
+    rpc: "https://proxies.sifchain.finance/api/stargaze-1/rpc",
+    rest: "https://proxies.sifchain.finance/api/stargaze-1/rest",
+    chainId: "stargaze-1",
+    chainName: "Stargaze",
+    stakeCurrency: {
+      coinDenom: "STARX",
+      coinMinimalDenom: "ustarx",
+      coinDecimals: 6,
+      coinGeckoId: "pool:ustarx",
+    },
+    walletUrl: "https://wallet.keplr.app/#/stargaze/stake",
+    walletUrlForStaking: "https://wallet.keplr.app/#/stargaze/stake",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "stars",
+      bech32PrefixAccPub: "starspub",
+      bech32PrefixValAddr: "starsvaloper",
+      bech32PrefixValPub: "starsvaloperpub",
+      bech32PrefixConsAddr: "starsvalcons",
+      bech32PrefixConsPub: "starsvalconspub",
+    },
+    currencies: [
+      {
+        coinDenom: "STARX",
+        coinMinimalDenom: "ustarx",
+        coinDecimals: 6,
+        coinGeckoId: "pool:ustarx",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "STARX",
+        coinMinimalDenom: "ustarx",
+        coinDecimals: 6,
+        coinGeckoId: "pool:ustarx",
+      },
+    ],
+    coinType: 118,
+    features: ["stargate", "ibc-transfer"],
+  },
+};
