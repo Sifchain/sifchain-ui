ceb206c767cc6e49911c6cd7faa466b98083a158
Alan <alanrsoars@gmail.com>
2022-06-08 15:13:39 +1200

    feat: add CMDX mainnet configs

---
+++ core/src/config/chains/comdex/comdex-mainnet.ts
@@ -0,0 +1,58 @@
+import { Network, IBCChainConfig } from "../../../entities";
+
+export const COMDEX_MAINNET: IBCChainConfig = {
+  chainType: "ibc",
+  network: Network.COMDEX,
+  displayName: "Comdex",
+  blockExplorerUrl: "https://www.mintscan.io/comdex",
+  nativeAssetSymbol: "ucmdx",
+  chainId: "comdex-1",
+  rpcUrl: "https://proxies.sifchain.finance/api/comdex-1/rpc",
+  restUrl: "https://proxies.sifchain.finance/api/comdex-1/rest",
+  features: {
+    erc20Transfers: true,
+  },
+  keplrChainInfo: {
+    rpc: "https://proxies.sifchain.finance/api/comdex-1/rpc",
+    rest: "https://proxies.sifchain.finance/api/comdex-1/rest",
+    chainId: "comdex-1",
+    chainName: "Comdex Testnet (Sifchain)",
+    stakeCurrency: {
+      coinDenom: "CMDX",
+      coinMinimalDenom: "ucmdx",
+      coinDecimals: 6,
+      coinGeckoId: "pool:ucmdx",
+    },
+    walletUrl: "https://wallet.keplr.app/#/cmdx-mainnet/stake",
+    walletUrlForStaking: "https://wallet.keplr.app/#/cmdx-mainnet/stake",
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "comdex",
+      bech32PrefixAccPub: "comdexpub",
+      bech32PrefixValAddr: "comdexvaloper",
+      bech32PrefixValPub: "comdexvaloperpub",
+      bech32PrefixConsAddr: "comdexvalcons",
+      bech32PrefixConsPub: "comdexvalconspub",
+    },
+    currencies: [
+      {
+        coinDenom: "CMDX",
+        coinMinimalDenom: "ucmdx",
+        coinDecimals: 6,
+        coinGeckoId: "pool:ucmdx",
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "CMDX",
+        coinMinimalDenom: "ucmdx",
+        coinDecimals: 6,
+        coinGeckoId: "pool:ucmdx",
+      },
+    ],
+    coinType: 118,
+    features: ["stargate", "ibc-transfer"],
+  },
+};
--- core/src/config/chains/comdex/index.ts
+++ core/src/config/chains/comdex/index.ts
@@ -2,10 +2,11 @@ import { NetworkEnv } from "../../getEnv";
 import { NetEnvChainConfigLookup } from "../NetEnvChainConfigLookup";
 
 import { COMDEX_TESTNET } from "./comdex-testnet";
+import { COMDEX_MAINNET } from "./comdex-mainnet";
 
 export default <NetEnvChainConfigLookup>{
   [NetworkEnv.LOCALNET]: COMDEX_TESTNET,
   [NetworkEnv.DEVNET]: COMDEX_TESTNET,
   [NetworkEnv.TESTNET]: COMDEX_TESTNET,
-  [NetworkEnv.MAINNET]: COMDEX_TESTNET,
+  [NetworkEnv.MAINNET]: COMDEX_MAINNET,
 };
