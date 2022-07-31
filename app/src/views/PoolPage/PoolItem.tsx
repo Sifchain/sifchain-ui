21bcdd9260e9c668d29fc2e2cd8950d3a4d097af
Alan Soares <alanrsoars@gmail.com>
2022-08-01 11:34:36 +1200

    chore: fix eslint errors

--- app/src/views/PoolPage/PoolItem.tsx
+++ app/src/views/PoolPage/PoolItem.tsx
@@ -124,13 +124,10 @@ export default defineComponent({
       );
     },
     externalAmount(): IAssetAmount {
-      return this.$props.pool.externalAmount!;
+      return this.$props.pool.externalAmount;
     },
     nativeAmount(): IAssetAmount {
-      return this.$props.pool.nativeAmount!;
-    },
-    formattedPoolStat() {
-      if (!this.$props.poolStat) return undefined;
+      return this.$props.pool.nativeAmount;
     },
     details(): [string, JSX.Element][] {
       if (!this.expanded) return []; // don't compute unless expanded
@@ -302,7 +299,7 @@ export default defineComponent({
               "flex items-center font-mono",
             ]}
           >
-            {!!this.userPoolData.myPoolShare?.value
+            {this.userPoolData.myPoolShare?.value
               ? `${parseFloat(this.userPoolData.myPoolShare.value).toFixed(2)}%`
               : ""}
           </div>
