de70d87f7745f706ea04e6ad01999b42d7b81c21
Alan Soares <alanrsoars@gmail.com>
2022-08-08 10:00:23 +1200

    chore: update margin fe url

--- app/src/components/NavSidePanel/NavSidePanel.tsx
+++ app/src/components/NavSidePanel/NavSidePanel.tsx
@@ -30,7 +30,7 @@ try {
   // do nothing
 }
 
-const MARGIN_FE_URL = "https://sifchain-dex.vercel.app/margin";
+const MARGIN_FE_URL = "https://sifchain-margin.vercel.app";
 
 export default defineComponent({
   setup() {
