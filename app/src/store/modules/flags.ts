42541352032b0ab1eb66a7ae33b810fd0c62efc4
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-08-17 17:52:47 +1200

    fix: move MARGIN_FE_URL to store/flags

--- app/src/store/modules/flags.ts
+++ app/src/store/modules/flags.ts
@@ -3,6 +3,8 @@ import { Chain, IAsset } from "@sifchain/sdk";
 
 import { Vuextra } from "../Vuextra";
 
+export const MARGIN_FE_URL = "https://sifchain-margin.vercel.app";
+
 export const isChainFlaggedDisabled = (chain: Chain) => {
   return (
     flagsStore.state.enableTestChains[
