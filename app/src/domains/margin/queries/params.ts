bc8772188df8f56f7978f8ea4c67d8ebcc2693d7
Alan Soares <alanrsoars@gmail.com>
2022-09-01 16:28:29 +1200

    feat: introduce useMarginEnabledPoolsQuery

--- app/src/domains/margin/queries/params.ts
+++ app/src/domains/margin/queries/params.ts
@@ -3,6 +3,7 @@ import { useQuery } from "vue-query";
 
 import { useSifchainClients } from "@/business/providers/SifchainClientsProvider";
 import dangerouslyAssert from "@/utils/dangerouslyAssert";
+import { useTokenRegistryEntriesQuery } from "@/domains/tokenRegistry/queries/tokenRegistry";
 
 export function useMarginParamsQuery() {
   const sifchainClients = useSifchainClients();
@@ -16,6 +17,37 @@ export function useMarginParamsQuery() {
     {
       enabled: computed(
         () => sifchainClients.queryClientStatus === "fulfilled",
+      ),
+    },
+  );
+}
+
+export function useMarginEnabledPoolsQuery() {
+  const marginParamsQuery = useMarginParamsQuery();
+  const tokenRegistryQuery = useTokenRegistryEntriesQuery();
+
+  return useQuery(
+    "marginEnabledPools",
+    () => {
+      dangerouslyAssert<"success">(marginParamsQuery.status.value);
+      dangerouslyAssert<"success">(tokenRegistryQuery.status.value);
+
+      const enabledPoolsDenoms =
+        marginParamsQuery.data.value?.params?.pools ?? [];
+
+      const tokenRegistryEntries =
+        tokenRegistryQuery.data.value?.registry?.entries ?? [];
+
+      const enabledPoolsEntries = tokenRegistryEntries.filter((entry) =>
+        enabledPoolsDenoms.includes(entry.denom),
+      );
+      return enabledPoolsEntries;
+    },
+    {
+      enabled: computed(
+        () =>
+          marginParamsQuery.isSuccess.value &&
+          tokenRegistryQuery.isSuccess.value,
       ),
     },
   );
